fe95329215b865d30f8c91aa35a2977d6f610df6
Sunday <sundaynetspend@gmail.com>
2022-07-04 03:22:11 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
-
-int main(void)
+/**
+*entry point :main
+*always return 0;
+*/
+int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
