982c9b7bbf56d0a8af1c188c920c5292c04aec51
Sunday <sundaynetspend@gmail.com>
2022-07-04 06:33:21 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+     *  main - print a text
+     *
+     * Return: always 0 success()
+     *
+     */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
