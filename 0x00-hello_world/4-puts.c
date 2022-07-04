99a59388d209fe92f54a1bdd0602d525638285f4
Sunday <sundaynetspend@gmail.com>
2022-07-04 05:16:27 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
-*entry point :main
-*always return 0;
-*/
-int main(void) 
+ * main - function
+ *
+ * Return: 0 if successful
+ *
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
