258642596e60d88654773f8e1c97325ce9c78981
Sunday <sundaynetspend@gmail.com>
2022-07-26 18:17:24 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,19 +14,17 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	if (size == 0)
-	{
 		return (NULL);
 
-		s = malloc(size * sizeof(char));
+	s = malloc(size * sizeof(char));
 
-		if (s == NULL)
-		{
-			return (NULL);
+	if (s == NULL)
+		return (NULL);
 
-			for (i = 0; i < size; i++)
-			{
-				s[i] = c;
-			}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
 
-			return (s);
-		}
+	return (s);
+}
